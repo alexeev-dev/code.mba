d42e8413d1ebddd0675232edee77f92cca203a2e
Vladimir Shalaev <vladimir-shalaev22@yandex.ru>
2016-10-21 22:17:34 +0700

    Подготовка структуры под скрипты

---
+++ app/js/main.js
@@ -0,0 +1,35 @@
+import loadElements from "utils/elements-loader";
+
+let MainController = {
+  /**
+   * Перечисляем хуки для выполнения различных действий контроллера
+   * Синтаксис: ["хук-класс", "действиеКонтроллера"]
+   */
+  hooks: [
+    ["js-open-search", "openSearch"]
+  ],
+
+  el: {
+    searchForm: ".overlay_search"
+  },
+
+  searchForm: {
+    state: {
+      open: "open"
+    }
+  },
+
+  init() {
+    this.hooks.forEach((descriptor) => {
+      let [hook, action] = descriptor;
+      $(`.${hook}`).click((event) => {
+        this[action]();
+      });
+    });
+  },
+
+  openSearch() {
+
+  }
+
+}
